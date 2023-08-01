0fe0a2cbf818a3dbfcf4f110c8a8c3b031141ff9
joonoo <ksop6580@naver.com>
2023-08-02 08:33:13 +0900

    Initial commit

---
+++ src/main/java/com/example/chatprac/ChatPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatPracApplication.class, args);
+    }
+
+}
